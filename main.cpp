b8c04e42e85e783e4959abbc811582816b265c9e
jacob-sasser <jacobsasser23@gmail.com>
2025-02-07 10:07:52 -0500

    m

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+
 #include <vector>
 #include <stdio>
-'''test'''
+#include <cmath>
+
+const double G=6.67 * pow(10,-11)
+class Body{
+ public:
+  double x;
+  double y;
+  double z;
+  double fx;
+  double fy;
+  double fz;
+  double vx;
+  double vy;
+  double vz;
+  double mass;
+  void update();
+
+} 
