d2f22cafeb4270ddd83344ddf994e031299d68d3
jacob-sasser <jacobsasser23@gmail.com>
2025-02-10 14:49:54 -0500

    body

--- Body.cpp
+++ Body.cpp
@@ -8,26 +8,22 @@ Body::Body(double x, double y, double z, double mass)
     : x(x), y(y), z(z), mass(mass), vx(0), vy(0), vz(0), fx(0), fy(0), fz(0) {}
 
 void Body::update(std::vector<Body>& bodies, double dt){
-  fx=fy=fz=0;
+  
   for(const auto& other : bodies){
     if(&other==this) continue;
-
-
-
-
 
   double dx=other.x-x;
   double dy=other.y-y;
   double dz=other.z-z;
 
-  double r= std::sqrt((dx*dx) + (dy*dy) +(dz*dz)+soft);
+  double r= std::sqrt((dx*dx) + (dy*dy) +(dz*dz)+(soft*soft));
   double r2=r*r;
   
   double f= G*((mass*other.mass)/r2);
-   fx= f*(dx/r);
-   fy= f*(dy/r);
-   fz= f*(dz/r);
-
+   fx+= f*(dx/r+soft);
+   fy+= f*(dy/r+soft);
+   fz+= f*(dz/r+soft);
+  }
   double ax=  fx/ mass;
   double ay= fy/ mass;
   double az= fz/ mass;
@@ -36,10 +32,9 @@ void Body::update(std::vector<Body>& bodies, double dt){
    vy=  vy+(ay*dt);
    vz=  vz + (az*dt);
 
-
    x= x+( vx*dt);
    y= y +( vy*dt);
    z= z +( vz*dt);
-      std::cout << "x, y, z: " <<  x << ", " <<  y << ", " <<  z << std::endl;
-    }//for loop
+ 
+    
 }
